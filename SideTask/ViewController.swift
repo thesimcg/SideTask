1b1886ff827a0e1bc71ea987808b036fc8ad30b1
Simon McGrath <simonmcgrath@live.com>
2021-11-18 16:05:00 +0000

    Initial Commit

---
+++ SideTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SideTask
+//
+//  Created by Simon McGrath on 18/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
